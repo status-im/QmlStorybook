b79ebe9b864d28897332b75c80c0f55c319fe1d8
Michał Cieślak <michalcieslak@status.im>
2025-07-31 13:10:13 +0200

    QmlFilesServer files version tracking added

--- include/Storybook/directorieswatcher.h
+++ include/Storybook/directorieswatcher.h
@@ -2,14 +2,11 @@
 
 #include <QObject>
 
-#include <QtQmlIntegration/qqmlintegration.h>
-
 class QFileSystemWatcher;
 
 class DirectoriesWatcher : public QObject
 {
     Q_OBJECT
-    QML_ELEMENT
 public:
     explicit DirectoriesWatcher(QObject *parent = nullptr);
     void addPaths(const QStringList &paths);
